16fb0cfa94f3adb4b029fe6177f8dfc1ada14a37
timabs <timur.abd.nyc@gmail.com>
2024-04-02 22:29:16 -0400

    got started on logic for editing a note. will hop over to backend to begin actual implementation with db

--- src/Components/HomeNotes/SingleNote.tsx
+++ src/Components/HomeNotes/SingleNote.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useEffect, useRef, useState } from "react";
 import { INote } from "../../@types/note";
 
 interface OneNoteProps {
@@ -7,14 +7,48 @@ interface OneNoteProps {
 }
 
 export const Note: FC<OneNoteProps> = ({ note, i }) => {
+  const [editMode, setEditMode] = useState<boolean>(false);
+  const contentRef = useRef<HTMLParagraphElement>(null);
+  const handleEdit = (id: number) => {
+    setEditMode(!editMode);
+  };
+  useEffect(() => {
+    if (editMode && contentRef.current) {
+      setTimeout(() => {
+        contentRef.current!.focus();
+      }, 0);
+    }
+  }, [editMode]);
   return (
     <div
-      className="border-2 border-black rounded-md p-2 w-full xl:w-1/3 flex flex-col min-h-48 justify-between h-1/3 max-h-52"
+      className={`border-2 border-black rounded-md p-2 w-full xl:w-1/3 flex flex-col min-h-48 justify-between h-1/3 max-h-52`}
       key={i}
+      data-id={note.id}
     >
       <div>
-        <h1 className="font-bold">{note.title}</h1>
-        <p className="break-words overflow-auto flex flex-col max-h-32 text-sm">
+        <div className="flex flex-row items-center justify-between w-full">
+          <h1
+            className={`font-bold flex flex-row justify-between p-2`}
+            contentEditable={editMode}
+          >
+            {note.title}{" "}
+          </h1>
+          <button
+            className={`font-normal border border-black rounded-md text-sm px-2 h-1/2`}
+            onClick={() => handleEdit(note.id)}
+          >
+            edit
+          </button>
+        </div>
+
+        <p
+          className={`${
+            editMode ? "outline-2 outline-black outline-solid" : "outline-none"
+          }break-words overflow-auto flex flex-col max-h-32 text-sm min-h-24 p-2`}
+          contentEditable={editMode}
+          ref={contentRef}
+          tabIndex={editMode ? 0 : -1}
+        >
           {note.content}
         </p>
       </div>
