4fe50c4e230ca65a69c470e200968efdb8bf04fd
timabs <timur.abd.nyc@gmail.com>
2024-04-19 00:26:51 -0400

    sidebar color adj

--- src/Components/Home/Sidebar.tsx
+++ src/Components/Home/Sidebar.tsx
@@ -5,7 +5,7 @@ const Sidebar = () => {
   const { sbOpen } = useContext(SidebarContext);
   return (
     <div
-      className={`h-screen absolute bg-gray-300 w-5/6 transition-all duration-300  ${
+      className={`h-screen absolute bg-gray-100 w-5/6 transition-all duration-300  ${
         sbOpen ? "left-0" : "-left-full"
       } xl:w-1/4`}
     ></div>
